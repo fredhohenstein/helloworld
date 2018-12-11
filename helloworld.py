92b79d9c25755ae1582a0f242d1389b2d8ea2281
Fred Hohenstein <fredhohenstein@users.noreply.github.com>
2018-12-11 15:10:36 +0100

    Versie 19

--- helloworld.py
+++ helloworld.py
@@ -9,18 +9,7 @@ import shutil
 from math import factorial
 from decimal import Decimal, getcontext
 
-def calc(n):
-    t= Decimal(0)
-    pi = Decimal(0)
-    deno= Decimal(0)
 
-    for k in range(n):
-        t = ((-1)**k)*(factorial(6*k))*(13591409+545140134*k)
-        deno = factorial(3*k)*(factorial(k)**3)*(640320**(3*k))
-        pi += Decimal(t)/Decimal(deno)
-    pi = pi * Decimal(12) / Decimal(640320 ** Decimal(1.5))
-    pi = 1/pi
-    return pi
 
 
 print("Hello World!")
@@ -41,4 +30,16 @@ time.sleep(2)
 # Chudnovsky algorithm for figuring out pi
 getcontext().prec=100
 n=5
+def calc(n):
+    t= Decimal(0)
+    pi = Decimal(0)
+    deno= Decimal(0)
+
+    for k in range(n):
+        t = ((-1)**k)*(factorial(6*k))*(13591409+545140134*k)
+        deno = factorial(3*k)*(factorial(k)**3)*(640320**(3*k))
+        pi += Decimal(t)/Decimal(deno)
+    pi = pi * Decimal(12) / Decimal(640320 ** Decimal(1.5))
+    pi = 1/pi
+    return pi
 print calc(n)
