fa220ec7e3e4f9b9ba3a2bfbdb4fa8e8a3972ed5
Fred Hohenstein <fredhohenstein@users.noreply.github.com>
2018-12-11 14:46:06 +0100

    versie 14

--- helloworld.py
+++ helloworld.py
@@ -7,7 +7,7 @@ from time import sleep
 import os  
 import shutil
 
-print("Hello World versie13!")
+print("Hello World versie14!")
 print("Sleeping for 5 seconds")
 time.sleep(5)
 
@@ -19,5 +19,5 @@ print(cwd)
 print("Sleeping for 2 seconds")
 time.sleep(2)
 
-with open("d:\runpyprograms\helloworld\version", "r") as f:
+with open("d:\\runpyprograms\\helloworld\\version", "r") as f:
     shutil.copyfileobj(f, sys.stdout)
